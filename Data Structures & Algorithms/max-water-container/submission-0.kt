fb95bea06e1385409656f8d8d2c6d9263ef780ed
Ankytm <ankytmaheshwari@gmail.com>
2026-08-13 22:43:31 +0530

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.kt
@@ -0,0 +1,29 @@
+class Solution {
+    fun maxArea(heights: IntArray): Int {
+
+        var res = 0 
+
+        var right = heights.size - 1
+        var left = 0 
+
+        while(left< right) {
+
+            val area = minOf(heights[left], heights[right]) *(right -left)
+
+            res = maxOf(res, area)
+
+            if(heights[left] <= heights[right]) {
+                left++
+            } else {
+                right --
+            }
+
+        }
+ 
+
+
+       return res
+        
+
+    }
+}
